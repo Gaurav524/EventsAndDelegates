0801172fd52c1c7548f2a216d3ed0cb2c9abff31
Gaurav Gupta <GGUPTA@volvocars.com>
2021-05-02 12:47:21 +0200

    Add project files.

---
+++ VideoEncoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        //1- Define a delegate
+        //2- Define an event based on that delegate
+        //3- Raise the event
+
+        /*Case 1: This is the old way to publish the event by defining delegate explicitly.
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;*/
+
+        /*Case 2: This is the new way (after .Net 3 or something) to publish the event
+        without defining delegate explicitly.
+        Both works same
+        EventHandler
+        EventHandler<TEventArgs>*/
+
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+        }
+    }
+}
